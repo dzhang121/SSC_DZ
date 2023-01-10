f061b7982d1e45dc5b6ce78ef907f03cefb7cff7
Ding Zhang <dzhang121@outlook.com>
2023-01-10 10:41:48 +0000

    January Assessment - Signals Part - Incomplete
    FFT plots to be modified

--- Signals_Lab/final_fusion.m
+++ Signals_Lab/final_fusion.m
@@ -3,8 +3,10 @@ close all
 clc
 
 %% magnetic compass setup
-tr=0.4;
-ts=18;
+% ref LAB guide P5-6
+
+tr=0.4; %rise time
+ts=18; %settling time
 k=1; %sensitivity
 
 w=1.02/tr;
@@ -17,13 +19,24 @@ c=1/w^2;
 e=0;
 d=k;
 
-%% heading input
-tend = 20;
-time = linspace(1,tend,1e3);
+%% heading input 
+
+
+tend = 140;
+
+t = 0:0.01:tend;
 % data = 100*ones(1,length(time));%constant
 % data = 100*sin(0.01*time); %low freq
-data = 100*sin(1*time);%high freq
-heading= [time' data'] ; 
+% data = 100*sin(1*time);%high freq
+
+data = 0.* (t>=0&t<=30)+sin(2*pi*t).*(t>30&t<32) + sin(pi*t).*(t>=32&t<34) +0.*(t>=34&t<=40) ;
+
+% figure;plot(t, data);
+heading= [t' data'] ; 
+
+%Used:
+heading_gain = 5;
+
 
 %cutoff freq
 omega0=1;
@@ -33,7 +46,9 @@ omega0=1;
 set_param('final_system', 'StopTime', num2str(tend))
 simout = sim('final_system.slx');
 
-%plot
+%% plot
+% figure;plot(simout.tout, simout.input)
+
 f1 = figure;
 tab1 = uitab('Title','Heading');
 axes('parent',tab1)
@@ -52,16 +67,82 @@ legend('Input',...
 
 tab2 = uitab('Title','Error');
 axes('parent',tab2)
+set(gca,'FontSize',11)
 hold on
 grid on
-plot(simout.tout, simout.fusion_out - simout.input)
-plot(simout.tout, simout.gyro_raw - simout.input)
-plot(simout.tout, simout.mag_raw - simout.input)
+plot(simout.tout, simout.fusion_out - simout.input,"LineWidth",2)
+plot(simout.tout, simout.gyro_raw - simout.input,"LineWidth",1)
+plot(simout.tout, simout.mag_raw - simout.input,"LineWidth",1)
 hold off
-legend('Error of Sensor Fusion',...
-    'Error of Gyro Compass',...
-    'Error of Magnetic Compass',...
-    'Interpreter','none')
+lgd = legend('Fused System',...
+    'Gyro Compass',...
+    'Magnetic Compass',...
+    'Interpreter','none');
+lgd.FontWeight = "bold";
+lgd.FontSize=11;
+xl = xlabel("Time (s)");
+yl = ylabel("Heading Error (°)");
+xl.FontWeight = "bold";
+xl.FontSize=12;
+yl.FontWeight = "bold";
+yl.FontSize=12;
+
+%% calculate RMS
+RMS=table;
+RMS.Fused = rms(simout.fusion_out - simout.input); 
+RMS.Gyro = rms(simout.gyro_raw - simout.input); 
+RMS.Magnetic = rms(simout.mag_raw - simout.input); 
 
 
+%% sweep break-frequency
+Q1b = table;
+i=0;
+% %sweep cutoff freq
+% for omega0=0:0.005:0.1
+%     i=i+1;
+%     clear simout
+%     set_param('final_system', 'StopTime', num2str(tend))
+%     simout = sim('final_system.slx');
+%     Q1b.bf(i) = omega0;
+%     %legacy - manual RMS calcs
+% %     Q1b.RMS_error_fused(i) = sqrt( sum((simout.fusion_out - simout.input).^2.*(140/length(simout.tout)))/140 );
+%     Q1b.RMS_error_fused(i) = rms(simout.fusion_out - simout.input);
+% end
 
+load("q1bdata.mat")
+figure;plot(Q1b.bf, Q1b.RMS_error_fused,"LineWidth",2)
+
+xl = xlabel("Break-frequency \omega (rad/s)");
+yl = ylabel("RMS Error of Fused System (°)");
+xl.FontWeight = "bold";
+xl.FontSize=10;
+yl.FontWeight = "bold";
+yl.FontSize=10;
+grid on
+
+%% FFT of magnetic compass
+% input
+T = max(simout.tout)/length(simout.tout);
+N = length(simout.tout);
+
+DFT = fft(simout.input);
+
+f = 0: 1/(N*T) : (N-1)/(N*T);
+
+DFT_corrected = 2* abs(DFT(1:N/2))/N;
+
+figure
+plot(f(1:N/2), DFT_corrected)
+hold on
+
+%output
+
+% subplot(2,1,2)
+
+DFT_m = fft(simout.mag_raw);
+
+DFT_m_corrected = 2* abs(DFT_m(1:N/2))/N;
+
+plot(f(1:N/2), DFT_m_corrected)
+
+legend('input','magnetic')
